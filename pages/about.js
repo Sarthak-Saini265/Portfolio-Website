ebfda29425c709103036ae19bc20141eeecb7e41
Sarthak Saini <saini.sarthak0505@gmail.com>
2024-11-10 22:18:56 +0530

    About page updated

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,51 @@
-import Image from 'next/image'
-import styler from '../styles/Home.module.css'
-import Link from 'next/link'
-import { Image } from 'next/image'
+import React, { useEffect, useState } from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import styler from "../styles/About.module.css";
 
+const About = () => {
 
-const about = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+    const [education, setEducation] = useState([])
 
+    useEffect(() => {
+        fetch("/api/aboutEducation").then((a) => {
+          return a.json()
+        }).then((res) => {
+          setEducation(res)
+        })
+      }, [])
 
-export default about
+  return (
+    <>
+        <div className={styler.wrapper}>
+            <section className={`${styler.education_sect} ${styler.sect_general}`}>
+                <div className={styler.section_head}>
+                    <h1>Education</h1>
+                </div>
+                <div className={styler.ed_divs}>
+                    {
+                        education.map((edu, ind) => {
+                            return <div className={`${styler.ed_div} ${edu.extra ? styler.ed_long_div : ''}`}>
+                                <div className={styler.logo_w_txt}>
+                                    <div className={styler.logo_div}>
+                                        <Image width={500} height={500} src={edu.img}/>
+                                    </div>
+                                        <h3>{edu.name}</h3>
+                                        {edu.extra ? <h3>{edu.extra}</h3> : null}
+                                        <h4>{edu.duration}</h4>
+                                </div>
+                                <div className={styler.info_div}>
+                                    <h3>{edu.program}</h3>
+                                    <p>{edu.description}</p>
+                                </div>
+                            </div>
+                        })
+                    }
+                </div>
+            </section>
+        </div>
+    </>
+  );
+};
 
-
-
-
-
+export default About;
