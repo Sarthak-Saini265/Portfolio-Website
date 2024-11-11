9e9dbbf3b015bfc98886037eb896e641c78cc17a
Sarthak Saini <saini.sarthak0505@gmail.com>
2024-11-11 20:08:20 +0530

    About page updated

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,8 @@ import styler from "../styles/About.module.css";
 const About = () => {
 
     const [education, setEducation] = useState([])
+    const [skills, setSkills] = useState([])
+    const [hobbies, setHobbies] = useState([])
 
     useEffect(() => {
         fetch("/api/aboutEducation").then((a) => {
@@ -13,7 +15,25 @@ const About = () => {
         }).then((res) => {
           setEducation(res)
         })
-      }, [])
+        }, [])
+
+    useEffect(() => {
+        fetch("/api/aboutSkills").then((a) => {
+            return a.json()
+        }).then((res) => {
+            setSkills(res)
+        })
+        }, [])
+    
+    useEffect(() => {
+        fetch("/api/aboutHobbies").then((a) => {
+            return a.json()
+        }).then((res) => {
+            setHobbies(res)
+        })
+        }, [])
+
+    
 
   return (
     <>
@@ -37,6 +57,54 @@ const About = () => {
                                 <div className={styler.info_div}>
                                     <h3>{edu.program}</h3>
                                     <p>{edu.description}</p>
+                                </div>
+                            </div>
+                        })
+                    }
+                </div>
+            </section>
+            <section className={`${styler.skill_sect} ${styler.sect_general}`}>
+                <div className={styler.section_head}>
+                    <h1>Skills</h1>
+                </div>
+                <duv className={styler.skill_divs}>
+                    {
+                        skills.map((skill, ind) => {
+                            return <div className={styler.skill_div}>
+                                <div style={skill.color ? {backgroundColor: skill.color} : {}} className={styler.skill_head}>{skill.category}</div>
+                                <ul>
+                                    {
+                                        skill.skills.map((skill_inner, ind) => {
+                                            return <li>
+                                                <div style={skill.color ? {backgroundColor: skill.color} : {}} class={styler.ico_div}>
+                                                    <Image
+                                                    src={skill_inner.img} width={500} height={500} style={skill_inner.name == "Vue JS" ? {width: "100%", height: "auto", filter: "grayscale(100%) brightness(3)"} : {}}/>
+                                                </div>
+                                                <p>{skill_inner.name}</p>
+                                            </li>
+                                        })
+                                    }
+                                </ul>
+                            </div>
+
+                        })
+                    }
+                </duv>
+            </section>
+            <section className={`${styler.hobbies_sect} ${styler.sect_general}`}>
+                <div className={styler.section_head}>
+                    <h1>Hobbies</h1>
+                </div>
+                <div className={styler.hobbies_divs}>
+                    {
+                        hobbies.map((hobby, ind) => {
+                            return <div className={styler.hobby_div}>
+                                <div>
+                                    <Image src={hobby.img} width={500} height={500}/>
+                                </div>
+                                <div className={styler.name_w_text}>
+                                    <h1>{hobby.name}</h1>
+                                    <p>{hobby.text}</p>
                                 </div>
                             </div>
                         })
